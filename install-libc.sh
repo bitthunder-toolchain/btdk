e908c5bcfe97ca2f062264a76922d0d85192bbec
James Walmsley <james@fullfat-fs.co.uk>
2013-11-19 17:00:18 +0100

    [BTDK] Updated the install-libc.sh script to copy the optimised libm etc libs.

--- install-libc.sh
+++ install-libc.sh
@@ -6,11 +6,9 @@ TARGET=${2}
 
 #find ${DIR}/build-newlib -name "libc.a" | sed "s|${DIR}/build-newlib/|${1}/|g" | sed "s|${TARGET}|${TARGET}/lib|g" | sed "s|newlib/||g" | grep -v libc/libc.a
 
-SOURCES=$(find ${DIR}/build-newlib -name "libc.a" | grep -v libc/libc.a)
-DESTS=$(echo ${SOURCES} | sed "s|${DIR}/build-newlib/|${1}/|g" | sed "s|${TARGET}|${TARGET}/lib|g" | sed "s|newlib/||g")
+SOURCES=$(find ${DIR}/build-newlib -name "libc.a" | grep -v libc/libc.a | sed "s|libc.a|*.a|g")
+DESTS=$(echo ${SOURCES} | sed "s|${DIR}/build-newlib/|${1}/|g" | sed "s|${TARGET}|${TARGET}/lib|g" | sed "s|newlib/||g" | sed "s|libc.a||g")
 
 while read src dst; do
 	sudo cp -v $src $dst
 done < <( paste <( echo $SOURCES | tr ' ' '\n'; ) <( echo $DESTS | tr ' ' '\n'; ); )
-
-
