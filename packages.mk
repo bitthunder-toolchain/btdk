9ed6c7df79d76706c4488622d0d6a1690a10d980
James Walmsley <james@fullfat-fs.co.uk>
2014-09-18 16:30:36 +0200

    [UBUNTU] Updated the default package list required to build on ubuntu.

--- packages.mk
+++ packages.mk
@@ -1,1 +1,1 @@
-PACKAGES=build-essential texinfo bison flex libgmp-dev libmpfr-dev libmpc-dev
+PACKAGES=build-essential texinfo bison flex libgmp-dev libmpfr-dev libmpc-dev libncurses5-dev zlib1g-dev
